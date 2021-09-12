5d57f2682c3c6ab013cc1a1b3cde2ebf62b91f1c
bNobo <benoit.rocco@gmail.com>
2021-09-12 10:01:41 +0200

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,11 @@
+echo "build..."
 ng build --prod
-ssh pi@192.168.1.25 "sudo systemctl stop windforecast-website && sudo rm -rf /home/pi/windforecast-website && mkdir /home/pi/windforecast-website"
-scp -r ./dist/wind-forecast-website/* pi@192.168.1.25:/home/pi/windforecast-website
-ssh pi@192.168.1.25 sudo systemctl start windforecast-website
+echo "backup..."
+ssh -i ~/.ssh/raspberry pi@192.168.1.25 'dirname="/home/pi/`date -I`-backup"; mkdir $dirname; sudo cp -r /home/pi/windforecast-website/* $dirname'
+echo "stop and clean..."
+ssh -i ~/.ssh/raspberry pi@192.168.1.25 "sudo systemctl stop windforecast-website && sudo rm -rf /home/pi/windforecast-website && mkdir /home/pi/windforecast-website"
+echo "copy files..."
+scp -i ~/.ssh/raspberry -r ./dist/wind-forecast-website/* pi@192.168.1.25:/home/pi/windforecast-website
+echo "start website..."
+ssh -i ~/.ssh/raspberry pi@192.168.1.25 sudo systemctl start windforecast-website
+echo "done."
